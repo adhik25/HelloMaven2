781c88dc5c547984b8baa4f365ec22c1cdf695a3
Adhi Kannan <adhithyak8@gmail.com>
2020-08-15 17:51:35 +1000

    First commit

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import org.pmw.tinylog.Logger;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven 2");
+        Logger.info("Hold me closer tiny logger");
+    }
+
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void Test1() {
+
+    }
+
+    @Test
+    public void Test2() {
+        Assert.assertTrue(true);
+    }
+}
