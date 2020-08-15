6a96e1b38509555e9a712d63c3cfa9641e4fa0ef
Adhi Kannan <adhithyak8@gmail.com>
2020-08-15 17:54:28 +1000

    Second commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Maven 2");
         Logger.info("Hold me closer tiny logger");
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Hello Maven 2");
+        }
     }
 
 }
